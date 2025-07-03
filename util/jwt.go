53e13d6551443a0216d20c836269e5e2183df8b0
Kevin Ongulu <kevin.ongulu@jubileekenya.com>
2025-07-03 09:46:00 +0300

    ExtractSubFromJWT

--- util/jwt.go
+++ util/jwt.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"crypto/rsa"
+	"fmt"
 	"log"
 	"os"
 
@@ -18,4 +19,20 @@ func LoadPublicKey(path string) *rsa.PublicKey {
 		panic("Invalid public key")
 	}
 	return key
+}
+
+func ExtractSubFromJWT(tokenString string) string {
+	token, _, err := new(jwt.Parser).ParseUnverified(tokenString, jwt.MapClaims{})
+	if err != nil {
+		fmt.Println("JWT parse error:", err)
+		return ""
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		if sub, ok := claims["sub"].(string); ok {
+			return sub
+		}
+	}
+
+	return ""
 }
