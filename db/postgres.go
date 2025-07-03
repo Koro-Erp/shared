0476b8e81f1d48472cd242bcd85e13e3d501e235
Kevin Ongulu <kevin.ongulu@jubileekenya.com>
2025-07-03 10:58:23 +0300

    DB connect

---
+++ db/postgres.go
@@ -0,0 +1,18 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+func Connect(dbUsername string, dbPassword string, dbName string,dbSSLMode string) *sql.DB {
+    connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=%s",
+        dbUsername, dbPassword, dbName, dbSSLMode)
+
+    db, err := sql.Open("postgres", connStr)
+    if err != nil {
+        log.Fatal("Error connecting to database:", err)
+    }
+    return db
+}
