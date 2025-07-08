d639713dc04e4c940603ad6078c0013641b38f4a
Kevin Ongulu <kevin.ongulu@jubileekenya.com>
2025-07-08 10:03:41 +0300

    add host and port to db connection

--- db/postgres.go
+++ db/postgres.go
@@ -7,13 +7,16 @@ import (
     _ "github.com/lib/pq"
 )
 
-func Connect(dbUsername string, dbPassword string, dbName string,dbSSLMode string) *sql.DB {
-    connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=%s",
-        dbUsername, dbPassword, dbName, dbSSLMode)
+func Connect(dbHost string, dbPort string, dbUsername string, dbPassword string, dbName string, dbSSLMode string) *sql.DB {
+    connStr := fmt.Sprintf(
+        "host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+        dbHost, dbPort, dbUsername, dbPassword, dbName, dbSSLMode,
+    )
 
     db, err := sql.Open("postgres", connStr)
     if err != nil {
         log.Fatal("Error connecting to database:", err)
     }
+
     return db
 }
