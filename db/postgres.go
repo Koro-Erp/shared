dfd6ba63d80a4cc16a97931559cc5a9d012a0a3a
Kevin Ongulu <kevin.ongulu@jubileekenya.com>
2025-07-09 16:24:13 +0300

    see connectiob string

--- db/postgres.go
+++ db/postgres.go
@@ -16,7 +16,7 @@ func Connect(dbConfig models.DbConfig) (*sql.DB, error) {
 		dbConfig.DBHost, dbConfig.DBPort, dbConfig.DBUser, dbConfig.DBPassword, dbConfig.DBName, dbConfig.DBSSLMode,
 	)
 
-	log.Println("Connecting to database...")
+	log.Println(connStr)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open database connection: %w", err)
