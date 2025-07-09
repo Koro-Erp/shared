19d6d3680ed1f5b18a973fcb5dacd03ab512c03c
Kevin Ongulu <kevin.ongulu@jubileekenya.com>
2025-07-09 12:25:43 +0300

    require token middleware

--- middleware/require_scope.go
+++ middleware/require_scope.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 )
@@ -33,5 +35,39 @@ func RequireScope(required string) gin.HandlerFunc {
 
 		c.JSON(403, gin.H{"code": 403, "message": "Missing required scope"})
 		c.Abort()
+	}
+}
+
+func RequireUserToken() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if _, ok := c.Get("user_claims"); !ok {
+			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "user token required"})
+			return
+		}
+		c.Next()
+	}
+}
+
+
+func RequireServiceToken() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if _, ok := c.Get("service_claims"); !ok {
+			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "service token required"})
+			return
+		}
+		c.Next()
+	}
+}
+
+
+func RequireBothTokens() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		_, userOK := c.Get("user_claims")
+		_, serviceOK := c.Get("service_claims")
+		if !userOK || !serviceOK {
+			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "both service and user tokens required"})
+			return
+		}
+		c.Next()
 	}
 }
