915be0a1ae24a3b9efd4daab07fa84fbe208cbc4
Kevin Ongulu <kevin.ongulu@jubileekenya.com>
2025-07-10 10:44:58 +0300

    SendGatewayLogRequest

--- handlers/core.go
+++ handlers/core.go
@@ -12,8 +12,40 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func SendLogRequest(c *gin.Context, data models.AppLog, url string) error {
+func SendAppLogRequest(c *gin.Context, data models.AppLog, url string) error {
 	// Marshal AppLog struct to JSON
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return fmt.Errorf("failed to marshal log data: %w", err)
+	}
+
+	// Create outbound POST request
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return fmt.Errorf("failed to create log request: %w", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	// Use the helper function to set token headers
+	util.CopyAuthHeaders(c, req)
+
+	// Send request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to send log request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	// Log response for debugging
+	// body, _ := io.ReadAll(resp.Body)
+	// fmt.Println("Log response:", string(body))
+
+	return nil
+}
+
+func SendGatewayLogRequest(c *gin.Context, data models.GatewayLog, url string) error {
+	// Marshal GatewayLog struct to JSON
 	jsonData, err := json.Marshal(data)
 	if err != nil {
 		return fmt.Errorf("failed to marshal log data: %w", err)
